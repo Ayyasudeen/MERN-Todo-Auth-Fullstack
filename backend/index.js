58ecf2c7669470b47926464f4947b3853fb0a4e8
ayyasudeen <126951002+AyyasudeenSimplain@users.noreply.github.com>
2023-08-02 12:58:27 +0530

    json, urlencoding and cookie parsing

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectionDB from "./database/connectionDB.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 dotenv.config();
 
 connectionDB();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on PORT ${process.env.PORT}`);
