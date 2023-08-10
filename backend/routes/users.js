fba97302fed98bf6f47d927e98ae53c20275aa50
ayyasudeen <126951002+AyyasudeenSimplain@users.noreply.github.com>
2023-08-10 22:00:39 +0530

    users routes added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,4 +2,18 @@ import express from "express";
 
 const router = express.Router();
 
+router.post("/register", register);
+
+router.post("/login", login);
+
+router.get("/logout", logout);
+
+router.get("/me", getMe);
+
+router.put("/updateDetails", updateDetails);
+
+router.put("/updatepassword", updatePassword);
+
+router.delete("/delete", deleteUser);
+
 export default router;
