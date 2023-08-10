e979eb0e53ae84c20509b189b798eb69ff3f3028
ayyasudeen <126951002+AyyasudeenSimplain@users.noreply.github.com>
2023-08-10 21:55:47 +0530

    routes for users and todos

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import connectionDB from "./database/connectionDB.js";
 import cookieParser from "cookie-parser";
 
+import todosRoutes from "./routes/todos.js"
+import usersRoutes from "./routes/users.js"
+
 const app = express();
 
 dotenv.config();
@@ -12,6 +15,9 @@ connectionDB();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+
+app.use("/api/todos", todosRoutes);
+app.use("/api/users", usersRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on PORT ${process.env.PORT}`);
