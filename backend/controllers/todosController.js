cc4dcfc66b8283e247d2be11ea0fc5a10f84e235
ayyasudeen <126951002+AyyasudeenSimplain@users.noreply.github.com>
2023-09-17 20:03:58 +0530

    getTodo

--- backend/controllers/todosController.js
+++ backend/controllers/todosController.js
@@ -1,6 +1,24 @@
 import Todo from "../models/Todo.js";
 
+
 export const getTodo = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const todo = await Todo.findById(id);
+        if(!todo) {
+            res.status(404).json({ msg: "Todo Not Found" });
+        }
+        if(todo.user.toString() !== req.user) {
+            res.status(401).json({ msg: "Not Auhtorized"});
+        }
+        res.status(200).json({ msg: "Todo Found", todo });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({errors: "Internal Server Error"});
+    }
+};
+
+export const getTodos = async (req, res) => {
     try {
         const todos = await Todo.find({user: req.user});
         res.status(200).json({msg: "Todo Found", todos})
@@ -9,8 +27,6 @@ export const getTodo = async (req, res) => {
         res.status(500).json({errors: "Internal Server Error"});
     }
 };
-
-export const getTodos = async (req, res) => {};
 
 export const createTodo = async (req, res) => {};
 
