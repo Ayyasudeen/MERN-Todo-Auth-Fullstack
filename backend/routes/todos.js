37731a13beec21157b59fe3720d8c1279d3f5bc2
ayyasudeen <126951002+AyyasudeenSimplain@users.noreply.github.com>
2023-09-17 19:57:12 +0530

    routes for the todos and todosController code

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,5 +1,17 @@
 import express from "express";
+import authorize from "../middleware/authorize.js";
+import { getTodo, getTodos, createTodo, updateTodo, deleteTodo } from "../controllers/todosController.js";
 
 const router = express.Router();
+
+router.get("/:id", authorize, getTodo);
+
+router.get("/", authorize, getTodos);
+
+router.post("/create", authorize, createTodo);
+
+router.put("/update/:id", authorize, updateTodo);
+
+router.delete("/delete/:id", authorize, deleteTodo);
 
 export default router;
