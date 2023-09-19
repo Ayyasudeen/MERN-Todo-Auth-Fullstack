d772574f1a92b642068a751b52388da038a9e0a3
ayyasudeen <126951002+AyyasudeenSimplain@users.noreply.github.com>
2023-09-19 12:46:45 +0530

    adding the rules and validateResult middlewares into the users routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,20 +1,22 @@
 import express from "express";
 import { register, login, logout, getMe, updateDetails, updatePassword, deleteUser} from "../controllers/usersController.js"
 import authorize from "../middleware/authorize.js";
+import { loginRules, registerRules, updateDetailsRules, updatePasswordRules } from "../middleware/validator.js";
+import { validateResult } from "../middleware/validationResults.js";
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", registerRules, validateResult, register);
 
-router.post("/login", login);
+router.post("/login", loginRules, validateResult, login);
 
 router.get("/logout", authorize, logout);
 
 router.get("/me", authorize, getMe);
 
-router.put("/updateDetails", authorize, updateDetails);
+router.put("/updateDetails", authorize, updateDetailsRules, validateResult, updateDetails);
 
-router.put("/updatepassword", authorize, updatePassword);
+router.put("/updatepassword", authorize, updatePasswordRules, validateResult, updatePassword);
 
 router.delete("/delete", authorize, deleteUser);
 
