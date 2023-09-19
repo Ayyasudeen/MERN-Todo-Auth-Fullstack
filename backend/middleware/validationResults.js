0179c8287490a2bebd743ca9ca12ebc1e1f4cc40
ayyasudeen <126951002+AyyasudeenSimplain@users.noreply.github.com>
2023-09-19 12:51:35 +0530

    Fix: validationResult

--- backend/middleware/validationResults.js
+++ backend/middleware/validationResults.js
@@ -1,7 +1,7 @@
 import { validationResult } from "express-validator";
 
 export const validateResult = (req, res, next) => {
-    const errors = validateResult(req);
+    const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({msg: errors.array()[0].msg});
     }
