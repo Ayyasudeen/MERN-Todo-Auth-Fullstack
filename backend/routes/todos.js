7bf1d9076614a974db36d43fb5436f4d9fabdad7
ayyasudeen <126951002+AyyasudeenSimplain@users.noreply.github.com>
2023-09-19 12:50:21 +0530

    adding the rules and validateResult middlewares into the todos routes

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,6 +1,8 @@
 import express from "express";
 import authorize from "../middleware/authorize.js";
 import { getTodo, getTodos, createTodo, updateTodo, deleteTodo } from "../controllers/todosController.js";
+import { createTodoRules, updateTodoRules } from "../middleware/validator.js";
+import { validateResult } from "../middleware/validationResults.js";
 
 const router = express.Router();
 
@@ -8,9 +10,9 @@ router.get("/:id", authorize, getTodo);
 
 router.get("/", authorize, getTodos);
 
-router.post("/create", authorize, createTodo);
+router.post("/create", authorize, createTodoRules, validateResult, createTodo);
 
-router.put("/update/:id", authorize, updateTodo);
+router.put("/update/:id", authorize, updateTodoRules, validateResult, updateTodo);
 
 router.delete("/delete/:id", authorize, deleteTodo);
 
