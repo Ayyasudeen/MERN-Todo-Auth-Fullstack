80033e931d1356267a31b27f2e0b673909e0279e
ayyasudeen <ayyasudeen@gmail.com>
2023-10-17 20:33:23 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    // port: 3000,
     proxy: {
       '/api': {
-        target: 'https://mern-todo-backend-rng7.onrender.com',
+        target: 'https://mern-todo-backend-tau.vercel.app',
         changeOrigin: true,
       },
     },
