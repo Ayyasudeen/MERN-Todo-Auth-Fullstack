54d0de2762d745e8fac93ca54a6494cf67c758bc
ayyasudeen <ayyasudeen@gmail.com>
2023-10-18 12:21:00 +0530

    Working Vite config for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,20 +8,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'mern-todo-backend-tau.vercel.app',
+        target: 'http://localhost:4000',
         changeOrigin: true,
-        secure: false,      
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', proxyReq.host, req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
       },
     },
   },
